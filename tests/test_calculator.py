33caa4c5bf2852439d0a582f14a6adb3bb000ae9
AlexanderS <alexsamindia@gmail.com>
2026-02-10 18:55:16 +0530

    added multiply feature

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,14 @@
 import pytest
-from app.calculator import add, divide, subtract
+from app.calculator import add, divide, multiply, subtract
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(4, 5) == 20
 
 def test_divide():
     assert divide(10, 2) == 5
